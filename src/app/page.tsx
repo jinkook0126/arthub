9a174fbd79e4e77063c07c60b5d855ef3878fe9c
이진국 <jinkook0126@naver.com>
2024-07-07 20:02:59 +0900

    feat: 허스키 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
-  let f = 1;
+  const f = 1;
   if (f == 1) {
-    console.log(123);
+    console.log(12344);
   }
 
   return (
