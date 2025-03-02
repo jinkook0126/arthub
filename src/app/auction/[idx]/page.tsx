6f4a37364630132f70b084aaddf4d892c6429044
이진국 <jinkook0126@naver.com>
2025-03-03 01:11:45 +0900

    feat: 상품 상세 진행중 #2
    - 레이아웃 수정

--- src/app/auction/[idx]/page.tsx
+++ src/app/auction/[idx]/page.tsx
@@ -4,24 +4,12 @@ function AuctionDetail() {
   return (
     <Box as='main' flexGrow={1} height='100%'>
       <Box maxW='1280px' mx='auto'>
-        <Flex as='article' pt='3rem' flexWrap='wrap' justify='space-between' pos='relative'>
-          {/* 왼쪽 컨텐츠 */}
-          <Box h='2000px' bg='green.50' flex='1' maxW='37.5rem'>
+        <Flex as='article' pt='3rem' flexWrap='wrap' justify='space-between' pos='relative' gap='80px'>
+          <Box h='2000px' bg='green.50' flex='1'>
             <Text>hello</Text>
           </Box>
-          {/* 오른쪽 sticky 영역 */}
-          {/* <Box w='21.125rem'>
-            <Box pos='sticky' top='0' zIndex={99}>
-              <Box bg='red.50'>
-                <Text>top</Text>
-              </Box>
-              <Box bg='blue.50'>
-                <Text>bottom</Text>
-              </Box>
-            </Box>
-          </Box> */}
-          <Box w='21.125rem'>
-            <Box pos='sticky' bottom='0' zIndex={99} height='300px' bg='yellow.100'>
+          <Box w='28rem' pos='sticky' bottom={0}>
+            <Box pos='sticky' top='3.625rem' overflowX='hidden' overflowY='scroll'>
               <Box bg='red.50'>
                 <Text>top</Text>
               </Box>
